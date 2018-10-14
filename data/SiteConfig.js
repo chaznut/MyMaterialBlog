f78464eefeba93bd9faa6f8f7436aeebe725d06d
James Nutter <nutterj90@gmail.com>
2018-10-14 12:01:11 -0400

    added GA

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -9,7 +9,7 @@ module.exports = {
   siteDescription: 'A blog written by a self-taught developer.', // Website description used for RSS feeds/meta description tag.
   siteRss: '/rss.xml', // Path to the RSS file.
   siteFBAppID: '1825356251115265', // FB Application ID for using app insights
-  siteGATrackingID: 'UA-47311644-4', // Tracking code ID for google analytics.
+  siteGATrackingID: 'UA-127422154-1', // Tracking code ID for google analytics.
   disqusShortname: 'https-chaznut-netlify-com.disqus.com/count.js', // Disqus shortname.
   postDefaultCategoryID: 'Tech', // Default category for posts.
   dateFromFormat: 'YYYY-MM-DD', // Date format used in the frontmatter.
