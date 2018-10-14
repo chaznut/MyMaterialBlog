b1d3014f538acbd4e05e371e95a476d9985041af
James Nutter <nutterj90@gmail.com>
2018-10-14 10:45:55 -0400

    updated readme

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -3,7 +3,7 @@ module.exports = {
   siteTitleShort: 'Nutter Blog', // Short site title for homescreen (PWA). Preferably should be under 12 characters to prevent truncation.
   siteTitleAlt: 'James Blog', // Alternative site title for SEO.
   siteLogo: '/logos/logo-1024.png', // Logo used for SEO and manifest.
-  siteUrl: 'https://vagr9k.github.io', // Domain of your website without pathPrefix.
+  siteUrl: 'https://chaznut.netlify.com/', // Domain of your website without pathPrefix.
   pathPrefix: '/gatsby-material-starter', // Prefixes all links. For cases when deployed to example.github.io/gatsby-material-starter/.
   fixedFooter: false, // Whether the footer component is fixed, i.e. always visible
   siteDescription: 'A blog written by a self-taught developer.', // Website description used for RSS feeds/meta description tag.
