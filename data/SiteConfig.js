99b546e2eec081a44dad53fc204e406100ced72d
James Nutter <nutterj90@gmail.com>
2018-11-25 21:51:07 -0500

    fixed bio

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -6,7 +6,7 @@ module.exports = {
   siteUrl: 'https://chaznut.netlify.com/', // Domain of your website without pathPrefix.
   pathPrefix: '/gatsby-material-starter', // Prefixes all links. For cases when deployed to example.github.io/gatsby-material-starter/.
   fixedFooter: false, // Whether the footer component is fixed, i.e. always visible
-  siteDescription: 'A blog written by a self-taught developer.', // Website description used for RSS feeds/meta description tag.
+  siteDescription: 'A blog written by a self-taught React/Node fullstack developer.', // Website description used for RSS feeds/meta description tag.
   siteRss: '/rss.xml', // Path to the RSS file.
   siteFBAppID: '1825356251115265', // FB Application ID for using app insights
   siteGATrackingID: 'UA-127422154-1', // Tracking code ID for google analytics.
@@ -18,7 +18,7 @@ module.exports = {
   userTwitter: '@theFlockaveli', // Optionally renders "Follow Me" in the UserInfo segment.
   userLocation: 'DMV Area, USA', // User location to display in the author segment.
   userAvatar: 'https://i.imgur.com/G6RMyCE.jpg', // User avatar to display in the author segment.
-  userDescription: 'Cats, coffee, and code.', // User description to display in the author segment.
+  userDescription: 'Fullstack developer currently using React/Redux/NodeJS/MongoDB. Cats, coffee, and code.', // User description to display in the author segment.
   // Links to social profiles/projects you want to display in the author segment/navigation bar.
   userLinks: [
     {
